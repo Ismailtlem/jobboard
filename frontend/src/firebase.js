89aa0b072545a9a7c4989f11efabb8f0e93d2e65
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2020-08-20 17:32:21 +0200

    commit

---
+++ frontend/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDzEpb2rZaylxFwemtlGn_n-3gqHLtsRtU",
+  authDomain: "joblisting-b53d5.firebaseapp.com",
+  databaseURL: "https://joblisting-b53d5.firebaseio.com",
+  projectId: "joblisting-b53d5",
+  storageBucket: "joblisting-b53d5.appspot.com",
+  messagingSenderId: "155126942062",
+  appId: "1:155126942062:web:27de69953319727accd97f",
+  measurementId: "G-YBJL7961GS"
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
