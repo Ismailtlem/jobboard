b62d30ee0865585427592f618813fbd6000a43c3
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2020-08-22 19:42:41 +0200

    added a banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import JobBoardComponent from "./components/JobBoardComponent";
 import data from "./assets/data.json";
 import SearchBar from "./components/SearchBar";
+import "./App.css";
 
 function App() {
   const [jobs, setJobs] = useState([]);
@@ -10,6 +11,9 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <div className="header">
+        <h1>The awesome job board for junior devs</h1>
+      </div>
       <h1 className="text-6xl"></h1>
       <SearchBar />;
       {jobs.length === 0 ? (
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "API_KEY",
+  apiKey: "AIzaSyDzEpb2rZaylxFwemtlGn_n-3gqHLtsRtU",
   authDomain: "joblisting-b53d5.firebaseapp.com",
   databaseURL: "https://joblisting-b53d5.firebaseio.com",
   projectId: "joblisting-b53d5",
   storageBucket: "joblisting-b53d5.appspot.com",
   messagingSenderId: "155126942062",
   appId: "1:155126942062:web:27de69953319727accd97f",
-  measurementId: "G-YBJL7961GS"
+  measurementId: "G-YBJL7961GS",
 });
 
 const db = firebaseApp.firestore();
