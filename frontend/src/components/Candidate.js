104fcd0967afd1853b3b8338038d56b48b96a496
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2020-09-14 10:41:45 +0200

    change freelancer image

--- frontend/src/components/Candidate.js
+++ frontend/src/components/Candidate.js
@@ -14,7 +14,7 @@ const Candidate = () => {
               <div className="mb-3">
                 <img
                   className="w-auto mx-auto rounded-full"
-                  src="https://i.pravatar.cc/150?img=66"
+                  src="./images/avatar-men.jpg"
                   alt=""
                 />
               </div>
@@ -40,7 +40,7 @@ const Candidate = () => {
               <div className="mb-3">
                 <img
                   className="w-auto mx-auto rounded-full"
-                  src="https://i.pravatar.cc/150?img=31"
+                  src="./images/avatar-women.jpg"
                   alt=""
                 />
               </div>
@@ -50,50 +50,6 @@ const Candidate = () => {
               <span className="text-blue-500 block mb-5">
                 Back End Developer
               </span>
-
-              <a
-                href="#"
-                className="px-4 py-2 bg-blue-500 text-white rounded-full"
-              >
-                Hire
-              </a>
-            </div>
-          </div>
-
-          {/*Card 3 */}
-          <div className="sm:w-1/4 p-2">
-            <div className="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
-              <div className="mb-3">
-                <img
-                  className="w-auto mx-auto rounded-full"
-                  src="https://i.pravatar.cc/150?img=18"
-                  alt=""
-                />
-              </div>
-              <h2 className="text-xl font-medium text-gray-700">Wayan Alex</h2>
-              <span className="text-blue-500 block mb-5">Data Scientist</span>
-
-              <a
-                href="#"
-                className="px-4 py-2 bg-blue-500 text-white rounded-full"
-              >
-                Hire
-              </a>
-            </div>
-          </div>
-
-          {/*Card 4 */}
-          <div className="sm:w-1/4 p-2">
-            <div className="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
-              <div className="mb-3">
-                <img
-                  className="w-auto mx-auto rounded-full"
-                  src="https://i.pravatar.cc/150?img=28"
-                  alt=""
-                />
-              </div>
-              <h2 className="text-xl font-medium text-gray-700">Ketut Julia</h2>
-              <span className="text-blue-500 block mb-5">Project Manager</span>
 
               <a
                 href="#"
