38aacf111ee973bc022e7a7fe2e3964c28308559
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2020-10-24 17:24:12 +0100

    correct-mistakes-backendfiles

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -19,29 +19,4 @@ const jobSchema = new Schema({
 
 const Job = mongoose.model("Job", jobSchema);
 
-/*
-var testJob = new Job({
-  company: "testCompany2",
-  logo:
-    "https://www.logodesign.net/logo/sea-swoosh-in-shape-of-letter-c-with-sun-4761ld.png",
-  jobNew: true,
-  featured: true,
-
-  position: "Full stack developer",
-  role: "Frontend",
-  level: "Senior",
-  postedAt: "4d ago",
-  contract: "Fulltime",
-  location: "Montreal",
-  languages: ["HTML", "CSS", "JavaScript"],
-  tools: [],
-});
-
-// save model to database
-testJob.save(function (err, done) {
-  if (err) return console.error(err);
-  console.log("inserted successfully");
-});
-*/
-
 module.exports = Job;
